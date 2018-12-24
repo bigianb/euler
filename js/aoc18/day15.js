16fd71e641996106598e99fe56e14d3470d7ad6f
Ian Brown <bigianb@gmail.com>
2018-12-24 13:14:18 +0000

    more day15

--- js/aoc18/day15.js
+++ js/aoc18/day15.js
@@ -62,7 +62,7 @@ function findUnits(map)
 
 function findEnemies(units, type)
 {
-    return units.filter(unit => unit.type != type);
+    return units.filter(unit => unit.type != type && unit.hp > 0);
 }
 
 
@@ -90,16 +90,17 @@ If multiple squares are in range and tied for being reachable in the fewest step
  After moving (or if the unit began its turn in range of a target), the unit attacks.
 */
 
-function findEnemyInRange(unit, enemies)
+function findEnemiesInRange(unit, enemies)
 {
+    let inRange = []
     for(enemy of enemies){
         if ((unit.y == enemy.y && (unit.x == enemy.x-1 || unit.x == enemy.x+1)) || 
             (unit.x == enemy.x && (unit.y == enemy.y-1 || unit.y == enemy.y+1))
         ){
-            return enemy;
+            inRange.push(enemy);
         }
     }
-    return null;
+    return inRange;
 }
 
 function isEmpty(x,y,map)
@@ -130,9 +131,54 @@ function findTargetSquares(enemies, map)
     return targets;
 }
 
-function findClosestTarget(unit, targets, map)
+function findBestMove(unit, targets, map)
 {
-    let closest = {targetIdx:0, route: []}
+    // There may be more than one closest route.
+    // In which case pick the one where the first move is closest in reading order.
+    // so, u, l, r, d
+    let bestMove = {dist:100000}
+    //u
+    if (unit.y > 0 && map[unit.y-1][unit.x] == '.'){
+        let closest = findClosestTarget(unit.x, unit.y-1, targets, map);
+        if (closest.valid){
+            bestMove.dist = closest.route.length;
+            bestMove.x = unit.x;
+            bestMove.y = unit.y-1;
+        }
+    }
+    //l
+    if (unit.x > 0 && map[unit.y][unit.x-1] == '.'){
+        let closest = findClosestTarget(unit.x-1, unit.y, targets, map);
+        if (closest.valid && closest.route.length < bestMove.dist){
+            bestMove.dist = closest.route.length;
+            bestMove.x = unit.x-1;
+            bestMove.y = unit.y;
+        }
+    }
+    //r
+    if (unit.x < map[unit.y].length - 1 && map[unit.y][unit.x+1] == '.'){
+        let closest = findClosestTarget(unit.x+1, unit.y, targets, map);
+        if (closest.valid && closest.route.length < bestMove.dist){
+            bestMove.dist = closest.route.length;
+            bestMove.x = unit.x+1;
+            bestMove.y = unit.y;
+        }
+    }
+    //d
+    if (unit.y < map.length - 1 && map[unit.y+1][unit.x] == '.'){
+        let closest = findClosestTarget(unit.x, unit.y+1, targets, map);
+        if (closest.valid && closest.route.length < bestMove.dist){
+            bestMove.dist = closest.route.length;
+            bestMove.x = unit.x;
+            bestMove.y = unit.y+1;
+        }
+    }
+    return bestMove;
+}
+
+function findClosestTarget(x, y, targets, map)
+{
+    let closest = {targetIdx:0, route: [], valid: false}
     
     let g = [];
     for (let row of map){
@@ -143,30 +189,40 @@ function findClosestTarget(unit, targets, map)
         }
         g.push(gRow);
     }
-    g[unit.y][unit.x]=1;
+    g[y][x]=1;
 
     var graph = new AStar.Graph(g);
     for (let targetIdx=0; targetIdx < targets.length; ++targetIdx){
-        var start = graph.grid[unit.y][unit.x];
         const tgt = targets[targetIdx];
-        var end = graph.grid[tgt.y][tgt.x];
-        var result = AStar.astar.search(graph, start, end);
-        const d = result.length;
-        if (d > 0){
-            if(closest.route.length == 0 || d.length < closest.route.length){
-                closest.route = result;
+        if (x == tgt.x && y == tgt.y){
+            closest.targetIdx = targetIdx;
+            closest.valid = true;
+            closest.route = [];
+            break;
+        } else {
+            var start = graph.grid[y][x];
+            var end = graph.grid[tgt.y][tgt.x];
+            var result = AStar.astar.search(graph, start, end);
+            const d = result.length;
+            if (d > 0){
+                if(!closest.valid || d.length < closest.route.length){    
+                    closest.route = result;
+                    closest.targetIdx = targetIdx;
+                    closest.valid = true;
+                }
             }
         }
     }
     return closest;
 }
 
-function moveTowardsTarget(unit, closestTarget, map)
+function moveTowardsTarget(unit, bestMove, map)
 {
-    if (closestTarget.route.length > 0){
-        // the a* library transposes x and y
-        unit.x = closestTarget.route[0].y;
-        unit.y = closestTarget.route[0].x;
+    if (bestMove.x != undefined){
+        map[unit.y][unit.x] = '.';
+        unit.x = bestMove.x;
+        unit.y = bestMove.y;
+        map[unit.y][unit.x] = unit.type;
     }
 }
 
@@ -179,41 +235,162 @@ function areWeDoneYet(map)
     return findEnemies(units, units[0].type).length == 0;
 }
 
-function attack(unit, enemy, map)
+function findweakestEnemy(enemiesInRange)
 {
+    let weakest=enemiesInRange[0];
+    for(let i=1; i<enemiesInRange.length; ++i){
+        let e = enemiesInRange[i];
+        if (e.hp < weakest.hp){
+            weakest = e;
+        } else if (e.hp == weakest.hp){
+            if ((e.y*1000+e.x) < (weakest.y*1000+weakest.x)){
+                weakest=e;
+            }
+        }
+    }
+    return weakest;
+}
+
+function attack(unit, enemiesInRange, map)
+{
+    let enemy = findweakestEnemy(enemiesInRange)
     enemy.hp -= unit.attack;
     if (enemy.hp <= 0){
         map[enemy.y][enemy.x] = '.';
     }
 }
 
-function run(map)
+// Need a mutable one
+function buildMap(map_in)
 {
+    let map = [];
+    for (let row of map_in){
+        let gRow = [];
+        for (let cell of row){
+            gRow.push(cell);
+        }
+        map.push(gRow);
+    }
+    return map;
+}
+
+function sumHp(units)
+{
+    let sum=0;
+    for (let unit of units){
+        sum += unit.hp;
+    }
+    return sum;
+}
+
+function run(map_in)
+{
+    let map=buildMap(map_in);
     let round=0;
     let units = findUnits(map);
     do {
-        for (let unit of units){
+        ++round;
+        units.sort(function(a,b){return (a.y*100+a.x) - (b.y*100+b.x)});
+        let partialRound=false;
+        for (let i=0; i < units.length; ++i){
+            let unit = units[i];
             if (unit.hp > 0){
                 let enemies = findEnemies(units, unit.type);
                 if (enemies.length != 0){
-                    let enemyInRange = findEnemyInRange(unit, enemies);
-                    if (!enemyInRange){
+                    let enemyInRange = findEnemiesInRange(unit, enemies);
+                    if (enemyInRange.length == 0){
                         let targets = findTargetSquares(enemies, map);
-                        let closestTarget = findClosestTarget(unit, targets, map);
-                        moveTowardsTarget(unit, closestTarget, map);
-                        enemyInRange = findEnemyInRange(unit, enemies);
+                        let bestMove = findBestMove(unit, targets, map);
+                        moveTowardsTarget(unit, bestMove, map);
+                        enemyInRange = findEnemiesInRange(unit, enemies);
                     }
-                    if (enemyInRange){
-                        attack(unit, enemy, map);
+                    if (enemyInRange.length > 0){
+                        attack(unit, enemyInRange, map);
                     }
+                } else {
+                    // done
+                    partialRound=true;
                 }
             }
         }
+        if (partialRound){
+            --round;
+        }
         // Remove any bodies
-        units = units.filter(unit => unit.hp <= 0);
-        ++round;
+        units = units.filter(unit => unit.hp > 0);
+
     } while (!areWeDoneYet(map));
-    return round;
+    let sum = sumHp(units);
+    console.log('sum='+sum+', round='+round)
+    return round*sum;
 }
 
 console.log(run(ex1Data));
+
+console.log(run(
+    ["#######",
+    "#G..#E#",
+    "#E#E.E#",
+    "#G.##.#",
+    "#...#E#",
+    "#...E.#" ,
+    "#######"]
+));
+console.log(run(
+    [
+    "#######",
+    "#E..EG#",
+    "#.#G.E#",
+    "#E.##E#",
+    "#G..#.#",
+    "#..E#.#" ,
+    "#######"]
+));
+
+/*
+Combat ends after 35 full rounds
+Goblins win with 793 total hit points left
+Outcome: 35 * 793 = 27755
+*/
+
+console.log(run(
+    [
+    "#######",
+    "#E.G#.#",
+    "#.#G..#",
+    "#G.#.G#",
+    "#G..#.#",
+    "#...E.#" ,
+    "#######"]
+));
+
+/*
+Combat ends after 54 full rounds
+Goblins win with 536 total hit points left
+Outcome: 54 * 536 = 28944
+*/
+console.log(run(
+    [
+    "#######",
+    "#.E...#",
+    "#.#..G#",
+    "#.###.#",
+    "#E#G#G#",
+    "#...#G#" ,
+    "#######"]
+));
+
+console.log(run(
+    [
+    "#########",  
+    "#G......#",  
+    "#.E.#...#",   
+    "#..##..G#",   
+    "#...##..#",  
+    "#...#...#",  
+    "#.G...G.#",  
+    "#.....G.#",  
+    "#########"]
+));
+
+console.log(run(data));
