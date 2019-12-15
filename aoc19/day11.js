7ad2d1a51c79d8ca3abf65ad5cef4085c0ae1dba
Ian Brown <bigianb@gmail.com>
2019-12-15 00:25:40 +0000

    aoc19 day11

---
+++ aoc19/day11.js
@@ -0,0 +1,288 @@
+const program_in = [3, 8, 1005, 8, 334, 1106, 0, 11, 0, 0, 0, 104, 1, 104, 0, 3, 8, 102, -1, 8, 10, 101, 1, 10, 10, 4, 10, 108, 0, 8, 10, 4, 10, 1002, 8, 1, 28, 2, 1108, 5, 10, 3, 8, 102, -1, 8, 10, 101, 1, 10, 10, 4, 10, 1008, 8, 1, 10, 4, 10, 1001, 8, 0, 55, 1, 102, 18, 10, 1, 2, 5, 10, 3, 8, 1002, 8, -1, 10, 1001, 10, 1, 10, 4, 10, 108, 1, 8, 10, 4, 10, 1001, 8, 0, 84, 1, 106, 11, 10, 2, 1008, 6, 10, 1, 4, 4, 10, 1006, 0, 55, 3, 8, 1002, 8, -1, 10, 1001, 10, 1, 10, 4, 10, 108, 0, 8, 10, 4, 10, 102, 1, 8, 121, 1, 107, 9, 10, 3, 8, 102, -1, 8, 10, 101, 1, 10, 10, 4, 10, 108, 1, 8, 10, 4, 10, 101, 0, 8, 147, 2, 1002, 4, 10, 2, 104, 18, 10, 1, 107, 16, 10, 1, 108, 8, 10, 3, 8, 102, -1, 8, 10, 101, 1, 10, 10, 4, 10, 108, 0, 8, 10, 4, 10, 102, 1, 8, 185, 3, 8, 1002, 8, -1, 10, 1001, 10, 1, 10, 4, 10, 1008, 8, 0, 10, 4, 10, 101, 0, 8, 208, 2, 1009, 16, 10, 1006, 0, 7, 1006, 0, 18, 1, 1105, 8, 10, 3, 8, 1002, 8, -1, 10, 101, 1, 10, 10, 4, 10, 108, 1, 8, 10, 4, 10, 101, 0, 8, 243, 2, 1105, 20, 10, 2, 106, 10, 10, 1006, 0, 67, 3, 8, 1002, 8, -1, 10, 101, 1, 10, 10, 4, 10, 108, 0, 8, 10, 4, 10, 1001, 8, 0, 276, 2, 1103, 5, 10, 2, 1104, 7, 10, 1006, 0, 35, 2, 1105, 3, 10, 3, 8, 1002, 8, -1, 10, 101, 1, 10, 10, 4, 10, 1008, 8, 1, 10, 4, 10, 1002, 8, 1, 314, 101, 1, 9, 9, 1007, 9, 1097, 10, 1005, 10, 15, 99, 109, 656, 104, 0, 104, 1, 21102, 936995824532, 1, 1, 21101, 0, 351, 0, 1105, 1, 455, 21102, 1, 387508445964, 1, 21102, 362, 1, 0, 1106, 0, 455, 3, 10, 104, 0, 104, 1, 3, 10, 104, 0, 104, 0, 3, 10, 104, 0, 104, 1, 3, 10, 104, 0, 104, 1, 3, 10, 104, 0, 104, 0, 3, 10, 104, 0, 104, 1, 21102, 1, 235244973059, 1, 21101, 409, 0, 0, 1106, 0, 455, 21102, 179410541659, 1, 1, 21101, 0, 420, 0, 1105, 1, 455, 3, 10, 104, 0, 104, 0, 3, 10, 104, 0, 104, 0, 21101, 868402070292, 0, 1, 21102, 1, 443, 0, 1106, 0, 455, 21102, 1, 709584749324, 1, 21102, 454, 1, 0, 1106, 0, 455, 99, 109, 2, 22102, 1, -1, 1, 21101, 40, 0, 2, 21102, 486, 1, 3, 21101, 0, 476, 0, 1106, 0, 519, 109, -2, 2105, 1, 0, 0, 1, 0, 0, 1, 109, 2, 3, 10, 204, -1, 1001, 481, 482, 497, 4, 0, 1001, 481, 1, 481, 108, 4, 481, 10, 1006, 10, 513, 1101, 0, 0, 481, 109, -2, 2106, 0, 0, 0, 109, 4, 2102, 1, -1, 518, 1207, -3, 0, 10, 1006, 10, 536, 21102, 0, 1, -3, 21202, -3, 1, 1, 22102, 1, -2, 2, 21102, 1, 1, 3, 21102, 555, 1, 0, 1106, 0, 560, 109, -4, 2106, 0, 0, 109, 5, 1207, -3, 1, 10, 1006, 10, 583, 2207, -4, -2, 10, 1006, 10, 583, 21201, -4, 0, -4, 1106, 0, 651, 21201, -4, 0, 1, 21201, -3, -1, 2, 21202, -2, 2, 3, 21102, 602, 1, 0, 1106, 0, 560, 22102, 1, 1, -4, 21101, 0, 1, -1, 2207, -4, -2, 10, 1006, 10, 621, 21102, 0, 1, -1, 22202, -2, -1, -2, 2107, 0, -3, 10, 1006, 10, 643, 21201, -1, 0, 1, 21102, 643, 1, 0, 106, 0, 518, 21202, -2, -1, -2, 22201, -4, -2, -4, 109, -5, 2106, 0, 0];
+
+function run(mem, input, pc, relativeBase) {
+    let output = [];
+
+    let inputIdx = 0;
+    let doBreak = false;
+    let status = 'halted';
+    while (mem[pc] != 99 && !doBreak) {
+        let oplen = 1;
+
+        let fullCode = mem[pc];
+        let opcode = fullCode % 100;
+        let sFullcode = new Intl.NumberFormat('en-GB', {
+            minimumIntegerDigits: 5,
+            maximumFractionDigits: 0,
+            useGrouping: false
+        }).format(fullCode);
+
+        // 1 == immediate mode
+        let aMode = sFullcode[0];
+        let bMode = sFullcode[1];
+        let cMode = sFullcode[2];
+
+        switch (opcode) {
+            case 0:
+                oplen = 4;
+                break;
+            case 1: {
+                let c = readVal(cMode, mem, pc + 1, relativeBase);
+                let b = readVal(bMode, mem, pc + 2, relativeBase);
+                writeVal(aMode, mem, pc + 3, relativeBase, c + b);
+                oplen = 4;
+            }
+            break;
+        case 2: {
+            let c = readVal(cMode, mem, pc + 1, relativeBase);
+            let b = readVal(bMode, mem, pc + 2, relativeBase);
+            writeVal(aMode, mem, pc + 3, relativeBase, c * b);
+            oplen = 4;
+        }
+        break;
+        case 3:
+            if (input.length <= inputIdx) {
+                status = 'needInput';
+                oplen = 0;
+                doBreak = true;
+            } else {
+                writeVal(cMode, mem, pc + 1, relativeBase, input[inputIdx]);
+                inputIdx += 1;
+                oplen = 2;
+            }
+            break;
+        case 4: {
+            let c = readVal(cMode, mem, pc + 1, relativeBase);
+            output.push(c);
+            status = 'output';
+            doBreak = true;
+            oplen = 2;
+        }
+        break;
+        case 5: {
+            // jump if true
+            let c = readVal(cMode, mem, pc + 1, relativeBase);
+            let b = readVal(bMode, mem, pc + 2, relativeBase);
+            if (c == 0) {
+                oplen = 3;
+            } else {
+                oplen = b - pc;
+            }
+        }
+        break;
+        case 6: {
+            // jump if false
+            let c = readVal(cMode, mem, pc + 1, relativeBase);
+            let b = readVal(bMode, mem, pc + 2, relativeBase);
+            if (c != 0) {
+                oplen = 3;
+            } else {
+                oplen = b - pc;
+            }
+        }
+        break;
+        case 7: {
+            /*
+            Opcode 7 is less than: if the first parameter is less than the second parameter,
+            it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+            */
+            oplen = 4;
+            let c = readVal(cMode, mem, pc + 1, relativeBase);
+            let b = readVal(bMode, mem, pc + 2, relativeBase);
+            if (c < b) {
+                writeVal(aMode, mem, pc + 3, relativeBase, 1);
+            } else {
+                writeVal(aMode, mem, pc + 3, relativeBase, 0);
+            }
+        }
+        break;
+        case 8: {
+            /*
+            Equals
+            */
+            oplen = 4;
+            let c = readVal(cMode, mem, pc + 1, relativeBase);
+            let b = readVal(bMode, mem, pc + 2, relativeBase);
+            if (c == b) {
+                writeVal(aMode, mem, pc + 3, relativeBase, 1);
+            } else {
+                writeVal(aMode, mem, pc + 3, relativeBase, 0);
+            }
+        }
+        break;
+        case 9: {
+            let c = readVal(cMode, mem, pc + 1, relativeBase);
+            relativeBase += c;
+            oplen = 2;
+        }
+        break;
+        default:
+            console.error('unknown opcode: ' + opcode + ' at index ' + pc);
+        }
+        pc += oplen;
+    }
+    return {
+        pc,
+        status,
+        output,
+        relativeBase
+    };
+}
+
+function readVal(mode, mem, idx, relativeBase) {
+    let rval = undefined;
+    if (mode === '0') {
+        rval = mem[mem[idx]];
+    } else if (mode === '1') {
+        rval = mem[idx];
+    } else if (mode === '2') {
+        rval = mem[mem[idx] + relativeBase];
+    } else {
+        console.log('unknown mode ' + mode);
+        rval = null;
+    }
+    if (rval === undefined) {
+        rval = 0;
+    }
+    return rval;
+}
+
+function writeVal(mode, mem, idx, relativeBase, val) {
+    if (mode === '0') {
+        mem[mem[idx]] = val;
+    } else if (mode === '1') {
+        console.error('attempt to write immediate address');
+    } else if (mode === '2') {
+        mem[mem[idx] + relativeBase] = val;
+    } else {
+        console.error('unknown mode ' + mode);
+    }
+}
+
+function readHull(x, y, hull) {
+    let key = '' + x + ',' + y;
+    if (key in hull) {
+        return hull[key]
+    } else {
+        return 0;
+    }
+}
+
+function writeHull(x, y, val, hull) {
+    let key = '' + x + ',' + y;
+    hull[key] = val;
+}
+
+function runProgram(program, hull) {
+    let pos = {
+        x: 0,
+        y: 0
+    }
+
+    let dir = 0;
+    let pc = 0;
+    let relativeBase = 0;
+    let done = false;
+    let mem = [...program];
+    let expectingColour = true;
+    do {
+        input = readHull(pos.x, pos.y, hull);
+        let out = run(mem, [input], pc, relativeBase);
+        done = out.status === 'halted';
+        pc = out.pc;
+        relativeBase = out.relativeBase;
+        if (out.status === 'output') {
+            if (expectingColour) {
+                writeHull(pos.x, pos.y, out.output[0], hull);
+                expectingColour = false;
+            } else {
+                dir = rotateRobot(dir, out.output[0]);
+                moveRobot(pos, dir);
+                expectingColour = true;
+            }
+        }
+    } while (!done);
+    return pos;
+}
+
+function rotateRobot(dir, rot) {
+    //  0 means it should turn left 90 degrees, and 1 means it should turn right 90 degrees.
+    // dir: 0=up, 1 right, 2 down, 3 left
+
+    if (rot === 1) {
+        dir += 1;
+    } else if (rot === 0){
+        dir += 3;
+    } else {
+        console.log('unknown rot: ' + rot)
+    }
+    return dir % 4;
+}
+
+function moveRobot(pos, dir) {
+    switch (dir) {
+        case 0:
+            pos.y += 1;
+            break;
+        case 1:
+            pos.x += 1;
+            break;
+        case 2:
+            pos.y -= 1;
+            break;
+        case 3:
+            pos.x -= 1;
+            break;
+        default:
+            console.error('dir out of range: '+dir);
+    }
+}
+
+function printHull(hull){
+    let bb = {x0: 100000, y0: 100000, x1: -100000, y1: -100000}
+    let whitePixels = []
+    for (let key of Object.keys(hull)){
+        if (hull[key] === 1){
+            let pos = key.split(',');
+            let x = Number(pos[0]);
+            let y = Number(pos[1]);
+            whitePixels.push({x, y})
+
+            bb.x0 = Math.min(x, bb.x0);
+            bb.y0 = Math.min(y, bb.y0);
+            bb.x1 = Math.max(x, bb.x1);
+            bb.y1 = Math.max(y, bb.y1);
+        }
+    }
+    let w = bb.x1 - bb.x0 + 1;
+    let h = bb.y1 - bb.y0 + 1;
+
+    let grid = [];
+    for (let y=h; y>=0; --y){
+        let row = "";
+        for (let x=0; x<w; ++x){
+            let hx = x + bb.x0;
+            let hy = y + bb.y0;
+            let val = readHull(hx, hy, hull);
+            if (val === 1){
+                row += 'x'
+            } else {
+                row += ' '
+            }
+        }
+        grid.push(row);
+    }
+
+    for(let row of grid){
+        console.log(row);
+    }
+}
+
+let hull = {'0,0': 1}
+runProgram(program_in, hull)
+console.log(Object.keys(hull).length);
+printHull(hull);
+
+
+
+console.log('done');
