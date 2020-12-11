f41c5fdc3f986ae9fe5cc6e83deb9ec6979e72af
Ian Brown <bigianb@gmail.com>
2020-12-11 18:23:19 +0000

    2019 day 24 pt 2

--- aoc/aoc19/js/day24pt2.js
+++ aoc/aoc19/js/day24pt2.js
@@ -25,12 +25,36 @@ const testInitial = {
 function getCellValue(x, y, map, edge)
 {
     if (x < 0 || x > 4 || y < 0 || y > 4){
-        return 0;
+        if (!map.up){
+            return 0;
+        }
+        if (x === -1){
+            return getSimpleCellValue(1, 2, map.up);
+        }
+        if (x === 5){
+            return getSimpleCellValue(3, 2, map.up);
+        }
+        if (y === -1){
+            return getSimpleCellValue(2, 1, map.up);
+        }
+        if (y === 5){
+            return getSimpleCellValue(2, 3, map.up);
+        }
+        console.error('unexpected x or y value');
     }
     if (x==2 && y == 2){
-        return getEdgeValue(map.down, edge);
+        if (map.down){
+            return getEdgeValue(map.down, edge);
+        } else {
+            return 0;
+        }
     }
-    return map[y][x] === '.' ? 0 : 1;
+    return getSimpleCellValue(x, y, map);
+}
+
+function getSimpleCellValue(x, y, map)
+{
+    return map.map[y][x] === '.' ? 0 : 1;
 }
 
 function getEdgeValue(map, edge)
@@ -43,22 +67,22 @@ function getEdgeValue(map, edge)
     {
         case 'T':
             for (let x=0; x<5; ++x){
-                count += getCellValue(x, 0, map);
+                count += getSimpleCellValue(x, 0, map);
             }
             break;
         case 'B':
             for (let x=0; x<5; ++x){
-                count += getCellValue(x, 4, map);
+                count += getSimpleCellValue(x, 4, map);
             }
             break;
         case 'L':
             for (let y=0; y<5; ++y){
-                count += getCellValue(0, y, map);
+                count += getSimpleCellValue(0, y, map);
             }
             break;
         case 'R':
             for (let y=0; y<5; ++y){
-                count += getCellValue(4, y, map);
+                count += getSimpleCellValue(4, y, map);
             }
             break;            
     }
@@ -76,25 +100,50 @@ function countNeighbours(x, y, map)
 
 function getNextGen(mapIn)
 {
+    let map = expandMap(mapIn);
+    // start at the top
+    while (map.up){ map = map.up}
+    let mapOut = emptyLevel();
+    let newTop = processSingleLevel(map);
+    mapOut.map = newTop;
+    while (map.down){
+        map = map.down;
+        let newGen = processSingleLevel(map);
+        mapOut.down = {
+            map: newGen,
+            up: mapOut,
+            down: null
+        }
+        mapOut = mapOut.down;
+    }
+    return mapOut;
+}
+
+function processSingleLevel(mapIn)
+{
     let mapOut=[]
     for(let row=0; row<5; ++row){
         let s=""
         for (let col=0; col<5; ++col){
-            let cv = getCellValue(col, row, mapIn);
-            let numNeighbours = countNeighbours(col, row, mapIn);
-            if (cv === 1){
-                // A bug dies (becoming an empty space) unless there is exactly one bug adjacent to it.
-                if (numNeighbours === 1){
-                    s += '#';
-                } else {
-                    s += '.';
-                }
+            if (row === 2 && col ===2){
+                s += '?'
             } else {
-                // An empty space becomes infested with a bug if exactly one or two bugs are adjacent to it.
-                if (numNeighbours === 1 || numNeighbours === 2){
-                    s += '#';
+                let cv = getCellValue(col, row, mapIn);
+                let numNeighbours = countNeighbours(col, row, mapIn);
+                if (cv === 1){
+                    // A bug dies (becoming an empty space) unless there is exactly one bug adjacent to it.
+                    if (numNeighbours === 1){
+                        s += '#';
+                    } else {
+                        s += '.';
+                    }
                 } else {
-                    s += '.';
+                    // An empty space becomes infested with a bug if exactly one or two bugs are adjacent to it.
+                    if (numNeighbours === 1 || numNeighbours === 2){
+                        s += '#';
+                    } else {
+                        s += '.';
+                    }
                 }
             }
         }
@@ -103,6 +152,79 @@ function getNextGen(mapIn)
     return mapOut;
 }
 
+function countBugs(map)
+{
+    while (map.up) {map = map.up}
+    let count = 0;
+    do {
+        count += countSingleLevel(map.map);
+        map = map.down;
+    } while (map);
+    return count;
+}
 
+function countSingleLevel(map)
+{
+    let count = 0;
+    for (let r=0; r<5; ++r){
+        for (let c=0; c<5; ++c){
+            if (r != 2 || c != 2){
+                if (map[r][c] === '#'){
+                    ++count;
+                }
+            }
+        }
+    }
+    return count;
+}
 
-console.log('done');
+function emptyLevel()
+{
+    return {
+        map: [
+            ".....",
+            ".....",
+            "..?..",
+            ".....",
+            "....."
+        ],
+        up: null,
+        down: null
+    }
+}
+
+function expandMap(map)
+{
+    while (map.up){
+        map = map.up;
+    }
+    let topBugs = countSingleLevel(map.map);
+    if (topBugs > 0){
+        map.up = emptyLevel();
+        map.up.down = map;
+    }
+    while (map.down){
+        map = map.down;
+    }
+    let bottomBugs = countSingleLevel(map.map);
+    if (bottomBugs > 0){
+        map.down = emptyLevel();
+        map.down.up = map;
+    }
+    return map;
+}
+
+function doPart2(map, time)
+{
+    let iteration = 0;
+    do {
+        map = getNextGen(map);
+        ++iteration;
+    } while (iteration < time)
+    return map;
+}
+
+let map = doPart2(initial, 200);
+while (map.up){map = map.up};
+console.log(countBugs(map));
+
