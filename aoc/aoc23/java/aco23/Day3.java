f5b65af077aa4b12087e535edb90ed984119c445
Ian Brown <bigianb@gmail.com>
2023-12-06 23:58:00 +0000

    AOC day 3

--- aoc/aoc23/java/aco23/Day3.java
+++ aoc/aoc23/java/aco23/Day3.java
@@ -9,7 +9,33 @@ public class Day3 {
         EngineSchematic exampleSchematic = new EngineSchematic();
         exampleSchematic.read(Path.of("inputs/day3_example.txt"));
 
+        int sum=0;
+        for (var pn : exampleSchematic.partNumbers){
+            sum += pn.num();
+        }
+        System.out.println("Example part 1 = " + sum);
 
+        var gears = exampleSchematic.getGears();
+        int ratios = 0;
+        for (var gear : gears){
+            ratios += gear.get(0).num() * gear.get(1).num();
+        }
+        System.out.println("Example part 2 = " + ratios);
+
+        EngineSchematic schematic = new EngineSchematic();
+        schematic.read(Path.of("inputs/day3.txt"));
+        sum=0;
+        for (var pn : schematic.partNumbers){
+            sum += pn.num();
+        }
+        System.out.println("Part 1 = " + sum);
+
+        gears = schematic.getGears();
+        ratios = 0;
+        for (var gear : gears){
+            ratios += gear.get(0).num() * gear.get(1).num();
+        }
+        System.out.println("Part 2 = " + ratios);
     }
 
 }
--- aoc/aoc23/java/aco23/EngineSchematic.java
+++ aoc/aoc23/java/aco23/EngineSchematic.java
@@ -47,14 +47,18 @@ public class EngineSchematic {
                         // first digit of a new part
                         curPartNo = digit;
                         x0 = x;
+                        x1 = x;
                     } else {
                         curPartNo = curPartNo * 10 + digit;
                         x1 = x;
                     }
                 } else {
                     if (curPartNo >= 0){
-                        var pn = new PartNo(curPartNo, x0, x1, y);
-                        partNumbers.add(pn);
+                        var sym = getNeighbouringSymbol(x0, x1, y);
+                        if (sym != 0) {
+                            var pn = new PartNo(curPartNo, x0, x1, y);
+                            partNumbers.add(pn);
+                        }
                     }
                     curPartNo = -1;
                     x0 = -1;
@@ -62,6 +66,58 @@ public class EngineSchematic {
                 }
             }
         }
-        System.out.println("Found " + partNumbers.size() + " part numbers");
+    }
+
+    public List<List<PartNo>> getGears()
+    {
+        List<List<PartNo>> gears = new ArrayList<>();
+        for (int y=0; y<height; ++y) {
+            for (int x = 0; x <= width; ++x) {
+                var c = getCharAt(x, y);
+                if (c == '*'){
+                    var parts = findNeighbouringParts(x, y);
+                    if (parts.size() == 2){
+                        gears.add(parts);
+                    }
+                }
+            }
+        }
+        return gears;
+    }
+
+    private List<PartNo> findNeighbouringParts(int x, int y) {
+        var parts = new ArrayList<PartNo>();
+        for (var part : partNumbers){
+            int l = part.x0 - 1;
+            int r = part.x1 + 1;
+            int t = part.y -1;
+            int b = part.y + 1;
+            if (x == l || x == r){
+                if (y >= t && y <= b){
+                    parts.add(part);
+                }
+            } else if (y == t || y == b){
+                if (x > l && x < r){
+                    parts.add(part);
+                }
+            }
+        }
+        return parts;
+    }
+
+    // x0 and x1 are inclusive
+    private char getNeighbouringSymbol(int x0, int x1, int y0) {
+        for (int y = y0 - 1; y <= y0 + 1; ++y){
+            for (int x = x0 - 1; x <= x1 + 1; ++x){
+                if (!(y == y0 && x >= x0 && x <= x1)){
+                    char c = getCharAt(x, y);
+                    if (!Character.isDigit(c) && c != '.') {
+                        return c;
+                    }
+                }
+            }
+        }
+
+        return 0;
     }
 }
